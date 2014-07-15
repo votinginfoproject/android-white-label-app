ea61e4df22dd356f6072824dad4d5566025f72bf
Andrew Fink <andrew.fink.11@gmail.com>
2014-07-15 09:32:08 -0400

    Add RetroLambda to project and fix use of HomeFragment
    App was skipping the use of HomeFragment entirely, called
    the HomeActivity onGoButtonPressed directly via the onClick
    xml attribute. When using a fragment with activity,
    ensure the following:
      - Activity extends android.support.v4.app.FragmentActivity
      - XML fragment definition has the class attribute properly set
      - Your Activity uses the activty xml layout

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/HomeActivity.java
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/HomeActivity.java
@@ -3,6 +3,7 @@ package com.votinginfoproject.VotingInformationProject.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,12 +11,13 @@ import android.view.View;
 import com.votinginfoproject.VotingInformationProject.R;
 import com.votinginfoproject.VotingInformationProject.fragments.HomeFragment;
 
-public class HomeActivity extends Activity implements HomeFragment.OnInteractionListener {
+public class HomeActivity extends FragmentActivity implements HomeFragment.OnInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+
+        setContentView(R.layout.activity_home);
     }
 
 
