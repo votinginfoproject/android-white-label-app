eef6d80c8f0a72d16cbfe4b37ce609fa6d50dfaa
CloudNiner <andrew.fink.11@gmail.com>
2014-07-29 18:05:36 -0400

    Add About Activity w/ Lorem Ipsum text

---
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.votinginfoproject.VotingInformationProject.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.votinginfoproject.VotingInformationProject.R;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
