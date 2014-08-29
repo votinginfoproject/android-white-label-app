c193bff3170b37908892f9049d6f864f4027ea87
Kathryn Killebrew <kkillebrew@azavea.com>
2014-08-29 15:37:38 -0400

    Add attribution for Google Maps

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/AboutActivity.java
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/AboutActivity.java
@@ -2,6 +2,9 @@ package com.votinginfoproject.VotingInformationProject.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.votinginfoproject.VotingInformationProject.R;
 
 public class AboutActivity extends Activity {
@@ -10,6 +13,11 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // add required attribution for Google Maps
+        TextView legalNoticesView = (TextView)this.findViewById(R.id.about_legal_notices);
+        String legalNotice = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(this);
+        legalNoticesView.setText(legalNotice);
     }
 
 }
