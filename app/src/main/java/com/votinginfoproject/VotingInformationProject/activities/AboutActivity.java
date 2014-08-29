f49aacd25ca6aa515e5983e9256cf5f1a4949d45
Kathryn Killebrew <kkillebrew@azavea.com>
2014-08-29 16:18:48 -0400

    Load open source license asynchronously.
    Otherwise the About view takes several seconds to load.

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/AboutActivity.java
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/activities/AboutActivity.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.votinginfoproject.VotingInformationProject.R;
+import com.votinginfoproject.VotingInformationProject.asynctasks.LoadOpenSourceLicense;
 
 public class AboutActivity extends Activity {
 
@@ -14,11 +14,9 @@ public class AboutActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        // add required attribution for Google Maps, prepended with app license
+        // asynchronously add required attribution for Google Maps, prepended with app license
         TextView legalNoticesView = (TextView)this.findViewById(R.id.about_legal_notices);
-        String legalNotice = GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(this);
-        String license = this.getResources().getString(R.string.about_license);
-        legalNoticesView.setText(license + legalNotice);
+        new LoadOpenSourceLicense().execute(legalNoticesView);
     }
 
 }
---
+++ app/src/main/java/com/votinginfoproject/VotingInformationProject/asynctasks/LoadOpenSourceLicense.java
@@ -0,0 +1,54 @@
+package com.votinginfoproject.VotingInformationProject.asynctasks;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.votinginfoproject.VotingInformationProject.models.VIPApp;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Load the open source license text for Google Maps attribution, and set it in the About text field.
+ * Doing this asynchronously because adding it to the view directly causes it to take several
+ * seconds to load.
+ *
+ * Created by kathrynkillebrew on 8/29/14.
+ */
+public class LoadOpenSourceLicense extends AsyncTask<TextView, Void, String> {
+    private WeakReference<TextView> textView = null;
+
+    protected String doInBackground(TextView... textViews) {
+        try {
+            this.textView = new WeakReference(textViews[0]);
+            // append the text to the end of what's already there (the app license)
+            StringBuilder builder = new StringBuilder(textViews[0].getText());
+            builder.append(GooglePlayServicesUtil.getOpenSourceSoftwareLicenseInfo(VIPApp.getContext()));
+            return builder.toString();
+        } catch (Exception ex) {
+            Log.e("LoadOpenSourceLicense", "Failed to asynchronously fetch Google Play services attribution");
+            ex.printStackTrace();
+        }
+
+        return "";
+    }
+
+
+    @Override
+    protected void onPostExecute(String text) {
+        if (textView != null) {
+            // get weakly referenced text view
+            TextView view = textView.get();
+            if (view != null && !text.isEmpty()) {
+                Log.d("LoadOpenSourceLicense:onPostExecute", "Setting text in view.");
+                view.setText(text);
+            } else {
+                Log.e("LoadOpenSourceLicense:onPostExecute", "Missing either view or text to set");
+            }
+        } else {
+            Log.e("LoadOpenSourceLicense:onPostExecute", "No TextView found to give the text.");
+        }
+    }
+}
