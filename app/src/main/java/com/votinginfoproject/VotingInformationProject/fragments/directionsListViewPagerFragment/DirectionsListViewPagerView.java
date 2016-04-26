5511fc541431eb1975ed6152a11b612397967082
Max Wade <max.wade@thirteen23.com>
2016-04-26 13:36:44 -0500

    Removed unused DirectionsListViewPagerFragment & associated classes

--- app/src/main/java/com/votinginfoproject/VotingInformationProject/fragments/directionsListViewPagerFragment/DirectionsListViewPagerView.java
+++
@@ -1,8 +0,0 @@
-package com.votinginfoproject.VotingInformationProject.fragments.directionsListViewPagerFragment;
-
-/**
- * Created by max on 4/21/16.
- */
-public interface DirectionsListViewPagerView {
-    void refresh();
-}
